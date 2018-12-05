a8de44e07aea01a318751e34fb682378e6f6bcba
Hang AmI <gradoc15@new.edvhtl.at>
2018-12-05 12:10:12 +0100

    added iniComp methods, (adds all labels and buttons in the correct order

--- Jg3_020_4Wins/src/Gui/GameGui.java
+++ Jg3_020_4Wins/src/Gui/GameGui.java
@@ -5,8 +5,13 @@
  */
 package Gui;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
@@ -15,12 +20,47 @@ import javax.swing.JFrame;
 public class GameGui extends JFrame
 {
 
+    private int row = 7;
+    private int col = 7;
+    
     public GameGui() throws HeadlessException
     {
         super.setTitle("4-Wins");
+        iniComp();
         super.setSize(600, 600);
         super.setLocationRelativeTo(null);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public void iniComp()
+    {
+       
+        Container con = this.getContentPane();
+        con.setLayout(new GridLayout(row, col, 3, 3));
+        
+        for(int i = 0; i < col; i++)
+        {
+            JButton btn = new JButton();
+            btn.setText("V");
+            con.add(btn);
+        }
+        
+        for(int i = 0; i < row-1; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                JLabel lb = new JLabel();
+                lb.setName(""+i+""+j);
+                lb.setText(""+i+""+j);
+                
+                lb.setOpaque(true);
+                lb.setBackground(Color.black);
+                
+                con.add(lb);
+            }
+        }
+        
+        
     }
     
     public static void main(String[] args)
