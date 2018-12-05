81b8c8f3ce9729ef79355582190c33f0eee6bf5f
Hang AmI <gradoc15@new.edvhtl.at>
2018-12-05 12:02:51 +0100

    added constructor and some standard things

--- Jg3_020_4Wins/src/Gui/GameGui.java
+++ Jg3_020_4Wins/src/Gui/GameGui.java
@@ -5,11 +5,27 @@
  */
 package Gui;
 
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
 /**
  *
  * @author User
  */
-public class GameGui
+public class GameGui extends JFrame
 {
+
+    public GameGui() throws HeadlessException
+    {
+        super.setTitle("4-Wins");
+        super.setSize(600, 600);
+        super.setLocationRelativeTo(null);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     
+    public static void main(String[] args)
+    {
+        GameGui gui = new GameGui();
+        gui.setVisible(true);
+    }
 }
