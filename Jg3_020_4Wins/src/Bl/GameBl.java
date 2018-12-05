1ccfe16a825ffa7ce17ca496e5945b589dca93fe
Hang AmI <gradoc15@new.edvhtl.at>
2018-12-05 12:28:34 +0100

    added makeMove

--- Jg3_020_4Wins/src/Bl/GameBl.java
+++ Jg3_020_4Wins/src/Bl/GameBl.java
@@ -11,5 +11,39 @@ package Bl;
  */
 public class GameBl
 {
+    private Value[][] field = new Value[7][6];
+    private Value currentPlayer;
+
+    public GameBl()
+    {
+        currentPlayer = Value.Player1;
+        
+        for(int i = 0; i < field.length; i++)
+        {
+            Value[] row = field[i];
+            
+            for(int j = 0; j < row.length; j++)
+            {
+                field[i][j] = Value.Empty;
+            }
+        }
+    }
+    
+    public void makeMove(int col)throws Exception
+    {
+        if(field[0][col].getNum() != Value.Empty.getNum())
+            throw new Exception("Invalid move, field is already used");
+        
+        for(int i = field.length-1; i > 0; i--)
+        {
+            field[i][col] = currentPlayer;
+        }
+        
+        if(currentPlayer == Value.Player1)
+            currentPlayer = Value.Player2;
+        else if(currentPlayer == Value.Player2)
+            currentPlayer = Value.Player1;
+    }
+    
     
 }
